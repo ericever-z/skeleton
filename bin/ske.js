3d81a7b0f678e1d0a84860bfaa64c12e699d4a7a
ericever-z <ericeverme@yahoo.com>
2017-09-04 17:36:20 +0800

    Update ske.js

--- bin/ske.js
+++ bin/ske.js
@@ -2,18 +2,20 @@
 
 var cmd    = require("commander"),
     pkg    = require("../package.json"),
-    readline     = require("readline")
-    ctrl = require("../lib/controller")
-    greeting = require("../lib/config/greeting");
+    readline     = require("readline"),
+	shell         = require("shelljs"),
+	help         = require("../lib/help"),
+	wizard        = require("../lib/wizard"),
+	Project      = require('../lib/project');
+	require("shelljs/global"),
     
     cmd
         .allowUnknownOption()
         .version(pkg.version)
-        .usage("<dir>,ske -w,ske -c")
-        .description("a tool for create a skeleton")
-        .option("-p,--plain <name>","plain project name")
-        .option("-s,--standard <name>","standard project name")//默认是 standard工程
-        .option("-a,--angular <name>","angular project name")//angular 框架下的工程
+        .usage("ske2 <project_name>,ske2 -c <project_name>")
+        .description("A tool for create a javascript project")
+        .option("-c,--custom <name>","a custom project")//默认是 standard工程
+        .option("-a,--angular <name>","an angular based project")//angular 框架下的工程
         .option("-l,--list","lists templates")
         .option("-d,--delete <project_name>","delete project");
     
@@ -21,68 +23,43 @@ var cmd    = require("commander"),
         console.log("   How to use:");
         console.log(new Array(20).join(" ") + "标准输入1：cmd options args");
         console.log(new Array(20).join(" ") + "标准输入2：cmd options");
-        console.log(new Array(20).join(" ") + "标准输入3：cmd subcmd");
         console.log(new Array(20).join(" "));
-        console.log("   Sample1:创建一个普通工程如下");
-        console.log(new Array(20).join(" ") + "ske -p hello_world");
+        console.log("   Sample: 创建一个hello_world工程");
+        console.log(new Array(20).join(" ") + "ske hello_world");
         
-    })
-    /**create project step by step
-        you need answer a few questions for helping customize your project
-    **/
-    cmd.command("wizard")
-       .alias("wiz")
-       .alias("custom")
-       .description("through wizard to create a project");  
-    
-    //create a empty standard project ---------------------  
-    cmd.command("standard")//sub command name
-       .alias("std") // sub command shot name
-       .description("create a standard project")  //sub command description
-       .option("-p,--pname","project name");
-
-    
-    //use template create a project------------------
-    cmd.command("template")
-        .alias("temp")
-       .description("choose a project template start a project");
-       
-    //subcommand help---------------------
-    cmd.command("help")
-       .description("子命令的帮助菜单")
-
-
+    });
 
 
    
     cmd.parse(process.argv);     
 
-    if(/^-./i.test(process.argv[2])){//如果是option
-        if(cmd.list){//显示项目模板
-            console.log("show list");
-        }
-        if(cmd.angular){
-            ctrl.angular(cmd.angular);
-        }else if(cmd.plain){//创建普通项目
-            ctrl.plain(cmd.plain);
-        }else if(cmd.standard){//创建标准项目
-            ctrl.stand(cmd.standard);
-        }else if(cmd.delete){//删除项目
-            ctrl.delete(cmd.delete);    
-        }
-     
-    }else if(cmd.args.length){//没有options
-        var subs = ["standard","std","wiz","wizard","custom","template","temp"];
-        var arg1 = cmd.args[0];//忽略后面的其他参数
-        var opts = process.argv.splice(3);
-        if(subs.indexOf(arg1)!=-1){//子命令
-            ctrl.decide({cmd:arg1,opts:opts});
-        }else{
-            ctrl.stand(cmd.args[0]);
-        }
-    }else{//没有任何参数
-        greeting().hello();// 显示欢迎界面
-        cmd.help();
-    }
-
-    
+  	
+    //判断地址栏输入的参数，容过地址栏有输入参数，无参数显示 使用方法。
+    if(cmd.rawArgs.length>2){
+		if(cmd.angular){
+			new Project(cmd.angular,"angular");//创建一个angular 项目
+		}else if(cmd.custom){
+			new wizard(cmd.custom);//创建一个用户自定义项目
+		}else if(cmd.list){
+			help.list(cmd.args[0]);  //打印项目目录结构
+		}else if(cmd.delete){
+			
+		}else{
+			var option = cmd.rawArgs[2];
+			if(/^\-[a-z]/ig.test(option)){
+				console.log("非法option ：" +option);
+			}else if(cmd.args){
+				if(cmd.args[0]=="wizard" || cmd.args[0]=="init"){
+					new wizard();
+				}else{
+					console.log("--------------------------------------------------");
+					new Project(cmd.args[0],"angular");////创建一个angular 项目
+				}
+			}
+		}
+		//console.log(cmd);
+	}else{
+		//console.log(cmd);
+		//console.log( pwd().stdout);
+		exec('ske2 --help');
+	} 
